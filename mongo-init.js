654bc9888cc06f984851ca58b27ad431313cb1b4
Helen85236 <79532870251@yandex.ru>
2023-11-16 22:01:53 +0300

    fix 2

--- mongo-init.js
+++ mongo-init.js
@@ -5,11 +5,11 @@ db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PA
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 
 db.createUser({
-    user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    pwd:  process.env.MONGO_INITDB_ROOT_PASSWORD,
+    user: process.env.MONGO_INITDB_USERNAME,
+    pwd:  process.env.MONGO_INITDB_PASSWORD,
     roles: [
       {
-        role: 'root',
+        role: 'readWrite',
         db: process.env.MONGO_INITDB_DATABASE,
       },
     ],
